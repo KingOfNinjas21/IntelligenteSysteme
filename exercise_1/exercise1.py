7390ccd2c25fc9ab9b4116839b1bc45b528fd26f
Patrick Hofmann <csav5085@student.uibk.ac.at>
2019-03-23 12:09:38 +0100

    Files hinzugefügt und rudimentär angepasst

---
+++ exercise_1/exercise1.py
@@ -0,0 +1,66 @@
+# Introduction to Intelligent and Autonomus Systems, UIBK, 2017
+#contact:senka.krivic@uibk.ac.at
+# 
+
+import vrep
+import numpy as np
+import time
+import math
+import movementFunctions as move
+
+
+def main():
+    print ('Program started')
+    vrep.simxFinish(-1) # just in case, close all opened connections
+    clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 2000, 5)
+
+    if clientID!=-1:
+        print ('Connected to remote API server')
+
+        # Start the simulation:
+        vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+
+
+        # start demo ---------------------------------------------------------------------------------------------------
+
+        print("startPos:")
+        move.printPos(clientID)
+
+        move.forward(2)
+        move.rotate(90)
+        move.printPos(clientID)
+
+        move.forward(2)
+        move.rotate(90)
+        move.printPos(clientID)
+
+        move.forward(2)
+        move.rotate(90)
+        move.printPos(clientID)
+
+        move.forward(2)
+        move.rotate(90)
+        move.printPos(clientID)
+
+        print("endPos:")
+        move.printPos(clientID)
+
+        # end demo -----------------------------------------------------------------------------------------------------
+
+
+        # Stop simulation:
+        vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+
+        # Now close the connection to V-REP:
+        vrep.simxFinish(clientID)
+    else:
+        print ('Failed connecting to remote API server')
+    print ('Program ended')
+
+
+
+def calcDistanceToTarget(xStart, yStart, xEnd, yEnd):
+    return math.sqrt((xEnd-xStart)*(xEnd-xStart)-(yEnd-yStart)*(yEnd-yStart))
+
+
+if __name__ == "__main__": main()
---
+++ exercise_1/movementFunctions.py
@@ -0,0 +1,150 @@
+import vrep
+import numpy as np
+import time
+import math
+
+# global variables for the youBot
+# B,C,D in meters
+B = 0.1
+C = 0.471
+D = 0.30046
+
+
+def getWheelJoints(clientID):
+    # Retrieve wheel joint handles:
+    wheelJoints = np.empty(4, dtype=np.int)
+    wheelJoints.fill(-1)  # front left, rear left, rear right, front right
+    res, wheelJoints[0] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_fl', vrep.simx_opmode_oneshot_wait)
+    res, wheelJoints[1] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_fr', vrep.simx_opmode_oneshot_wait)
+    res, wheelJoints[2] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_rl', vrep.simx_opmode_oneshot_wait)
+    res, wheelJoints[3] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_rr', vrep.simx_opmode_oneshot_wait)
+
+    return wheelJoints
+
+
+def customMovement(time_arg, clientID, forw_back_vel, left_right_vel, rotvel):
+    wheelVelocities = wheelVel(forw_back_vel, left_right_vel, rotvel)
+    startMovement(time_arg, clientID, wheelVelocities)
+
+def forward(meter):
+    return
+
+def rotate(degree):
+    return 
+
+
+def forward(speed, distance, clientID):
+    print ("Begin forward at ")
+    printPos(clientID)
+    wheelJoints = getWheelJoints(clientID)
+    for i in range(0, 4):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], 0, vrep.simx_opmode_oneshot)
+
+    vrep.simxPauseCommunication(clientID, True)
+    for i in range(0, 4):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], wheelVel(-speed, 0.0, 0.0)[i],
+                                        vrep.simx_opmode_oneshot)
+    vrep.simxPauseCommunication(clientID, False)
+
+    distanceTravelled = 0
+    dt = 0.0
+    while distanceTravelled < distance:
+        start = time.time()
+        x, y, w = odometry(0.0, 0.0, 0.0, 4.0, 0.0, 0.0, dt)
+        distanceTravelled+=math.sqrt(x*x+y*y)
+        #printPos(clientID)
+        end = time.time()
+        dt = end - start
+
+    for i in range(0, 4):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], 0, vrep.simx_opmode_oneshot)
+    print ("End forward at ")
+    printPos(clientID)
+
+
+def rotateDegrees(degrees, clientID, rotateSpeed):
+    rad = degrees * math.pi / 180.0
+    tempTime = rad / rotateSpeed
+    print(tempTime)
+    wheelVelocities=calcVelocitiesForRotation(rotateSpeed)
+    startMovement(tempTime, clientID, wheelVelocities)
+
+
+def calcVelocitiesForRotation(rotateSpeed):
+    #what C and D is, have a look at YouBotDetailedSpecifiations
+
+    R = 50.0 #radius of one wheel
+    L1 = 150.23 # D/2
+    L2 = 235.5 # C/2
+    vx = 0 #speedForXandY
+    vy = 0 #speedForXandY
+
+    vel = [0.0]*4
+    vel[0] = 1 / R * (1 * vx + 1 * vy - (L1 + L2) * rotateSpeed) # front left
+    vel[1] = 1 / R * (1 * vx - 1 * vy + (L1 + L2) * rotateSpeed) # front right
+    vel[2] = 1 / R * (1 * vx - 1 * vy - (L1 + L2) * rotateSpeed) # rear left
+    vel[3] = 1 / R * (1 * vx + 1 * vy + (L1 + L2) * rotateSpeed) # rear right
+    print (vel)
+    return vel
+
+
+def startMovement(time_arg, clientID, wheelVelocities):
+    # initialize robot
+    wheelJoints = getWheelJoints(clientID)
+
+    # set wheel velocity to 0
+    for i in range(0, 4):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], 0, vrep.simx_opmode_oneshot)
+
+    vrep.simxPauseCommunication(clientID, True)
+    for i in range(0, 4):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], wheelVelocities[i], vrep.simx_opmode_oneshot)
+    vrep.simxPauseCommunication(clientID, False)
+    time.sleep(time_arg)
+
+    # set wheel velocity to 0
+    for i in range(0, 4):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], 0, vrep.simx_opmode_oneshot)
+
+
+def printPos(clientID):
+    res, base = vrep.simxGetObjectHandle(clientID, 'youBot_center', vrep.simx_opmode_oneshot_wait)
+    base_pos = vrep.simxGetObjectPosition(clientID, base, -1, vrep.simx_opmode_oneshot_wait)
+    base_orient = vrep.simxGetObjectOrientation(clientID, base, -1, vrep.simx_opmode_oneshot_wait)
+    vrep.simxGetPingTime(clientID)  # make sure that all streaming data has reached the client at least once
+
+    print("Position: ", base_pos[1])
+    print("Orientation: ", base_orient[1])
+
+def getPos(clientID):
+    res, base = vrep.simxGetObjectHandle(clientID, 'youBot_center', vrep.simx_opmode_oneshot_wait)
+    base_pos = vrep.simxGetObjectPosition(clientID, base, -1, vrep.simx_opmode_oneshot_wait)
+    base_orient = vrep.simxGetObjectOrientation(clientID, base, -1, vrep.simx_opmode_oneshot_wait)
+    return base_pos[1], base_orient[1]
+
+def wheelVel(forwBackVel, leftRightVel, rotVel):
+    return np.array([-forwBackVel-leftRightVel-rotVel, -forwBackVel+leftRightVel-rotVel, -forwBackVel+leftRightVel+rotVel, -forwBackVel-leftRightVel+rotVel])
+
+
+# this function calculates the new position of the robot based on the old position and the speed of every wheel and the time difference
+def odometry(x, y, w, forwBackVel, leftRightVel, rotVel, dt):
+    vf, vr, w0 = formatVel(forwBackVel, leftRightVel, rotVel)
+
+    o = w0 * dt
+    dx = -vf * dt * math.sin(o) + vr * dt * math.cos(o)
+    dy = vf * dt * math.cos(o) + vr * dt * math.sin(o)
+
+    x = x + dx
+    y = y + dy
+    w = w + o
+
+    return x, y, w
+
+
+# This function formats the spin of every wheel to a forward-, leftRightvelocety and an angle
+def formatVel(forwBackVel, leftRightVel, rotVel):
+    vf = forwBackVel * B/2.0
+    vr = leftRightVel * B/2.0
+    w0 = 2.0/(C+D) * rotVel * B/2.0
+
+    return vf, vr, w0
