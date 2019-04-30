25e95bb960863b9502cccfc3fd5e1749bf16d308
Paddy <csav5085@student.uibk.ac.at>
2019-04-30 21:15:45 +0200

    refactoring 2. part

---
+++ exercise_4/colorDetection.py
@@ -0,0 +1,27 @@
+import array
+import numpy as np
+import cv2
+
+from PIL import Image
+
+
+'''
+converts an image from vrep sensor to cv2 format.
+sesorImg: the image from the sensor
+res: the resolution as array
+'''
+def convertToCv2Format (sensorImg, res):
+    # transformation to byte array
+    image_byte_array = array.array('b', sensorImg)
+
+    # transformation to opencv2 image
+    image_buffer = Image.frombuffer("RGB", (res[0], res[1]), np.asarray(image_byte_array), "raw", "RGB", 0, 1)
+    img = np.asarray(image_buffer)
+
+    # Convert RGB to BGR and flip
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.flip(img, 0)
+
+    return img
+
+
