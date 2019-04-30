144345f21ab983e04e00c0c2bde323be68398145
csav4821 <sebastian.horngacher@student.uibk.ac.at>
2019-04-30 12:56:18 +0200

    first steps witch cv2 in testInput

---
+++ exercise_4/testInput.py
@@ -0,0 +1,34 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+
+# load the image
+image = cv2.imread("color_detection_blue_version.jpg")
+
+# define the list of boundaries
+boundaries = [([17, 15, 100], [50, 56, 200])]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+    # show the images
+    cv2.imshow("images", np.hstack([image, output]))
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.waitKey(0)
