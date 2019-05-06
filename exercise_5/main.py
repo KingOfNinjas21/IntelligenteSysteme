78414f7a0c5996097e5f6d68117a7b6c41d20bb4
csav4821 <sebastian.horngacher@student.uibk.ac.at>
2019-05-06 12:12:33 +0200

    created new folder for exercise5 and copied files from the last exercise  in here

---
+++ exercise_5/main.py
@@ -0,0 +1,77 @@
+# Introduction to Intelligent and Autonomus Systems, UIBK, 2017
+#contact:senka.krivic@uibk.ac.at
+#
+
+import vrep
+import numpy as np
+import cv2
+import time
+import math
+import colorDetection as colorDet
+
+goalName = "Goal"
+LEFT_RAY_NINETY = 603
+RIGHT_RAY_NINETY = 82
+
+def main():
+    print ('Program started')
+    emptybuf = bytearray()
+
+    vrep.simxFinish(-1) # just in case, close all opened connections
+    clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 2000, 5)
+
+
+    if clientID!=-1:
+        print ('Connected to remote API server')
+
+        # Start the simulation:
+        vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+
+        # start init wheels --------------------------------------------------------------------------------------------
+
+        wheelJoints = np.empty(4, dtype=np.int)
+        wheelJoints.fill(-1)  # front left, rear left, rear right, front right
+        res, wheelJoints[0] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_fl', vrep.simx_opmode_oneshot_wait)
+        res, wheelJoints[1] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_rl', vrep.simx_opmode_oneshot_wait)
+        res, wheelJoints[2] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_fr', vrep.simx_opmode_oneshot_wait)
+        res, wheelJoints[3] = vrep.simxGetObjectHandle(clientID, 'rollingJoint_rr', vrep.simx_opmode_oneshot_wait)
+
+        # end init wheels ----------------------------------------------------------------------------------------------
+
+        # start init camera --------------------------------------------------------------------------------------------
+
+        # change the angle of the camera view (default is pi/4)
+        res = vrep.simxSetFloatSignal(clientID, 'rgbd_sensor_scan_angle', math.pi / 2, vrep.simx_opmode_oneshot_wait)
+
+        # turn on camera
+        res = vrep.simxSetIntegerSignal(clientID, 'handle_rgb_sensor', 2, vrep.simx_opmode_oneshot_wait);
+
+        # get camera object-handle
+        res, youBotCam = vrep.simxGetObjectHandle(clientID, 'rgbSensor', vrep.simx_opmode_oneshot_wait)
+
+        # get first image
+        err, resolution, image = vrep.simxGetVisionSensorImage(clientID, youBotCam, 0, vrep.simx_opmode_streaming)
+        time.sleep(1)
+
+        # end init camera ----------------------------------------------------------------------------------------------
+
+
+
+        # programmable space -------------------------------------------------------------------------------------------
+
+        colorDet.exercise4_action(clientID, youBotCam)
+
+        # end of programmable space --------------------------------------------------------------------------------------------
+
+
+
+        # Stop simulation
+        vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+
+        # Close connection to V-REP:
+        vrep.simxFinish(clientID)
+    else:
+        print ('Failed connecting to remote API server')
+    print ('Program ended')
+
+if __name__ == "__main__": main()
