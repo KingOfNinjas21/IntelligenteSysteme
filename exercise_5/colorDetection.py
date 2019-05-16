ec95a0515e6847ddbbbb53d9531bf5d61d95d0a2
csav4821 <sebastian.horngacher@student.uibk.ac.at>
2019-05-16 12:34:35 +0200

    merge

--- exercise_5/colorDetection.py
+++ exercise_5/colorDetection.py
@@ -6,18 +6,22 @@ import movementFunctions as move
 
 from PIL import Image
 
-
 # constans for color boundaries
 boundariesGreen = ([50, 200, 50], [75, 255, 75])
+boundariesYellow = ([50, 210, 210], [80, 255, 255])
+boundariesBlue = ([204, 45, 45], [255, 85, 85])
+boundariesRed = ([45, 45, 205], [86, 86, 255])
 
-
+colors = [boundariesRed, boundariesYellow, boundariesBlue, boundariesGreen]
 
 '''
 converts an image from vrep sensor to cv2 format.
 sesorImg: the image from the sensor
 res: the resolution as array
 '''
-def convertToCv2Format (sensorImg, res):
+
+
+def convertToCv2Format(sensorImg, res):
     # transformation to byte array
     image_byte_array = array.array('b', sensorImg)
 
@@ -31,45 +35,71 @@ def convertToCv2Format (sensorImg, res):
 
     return img
 
+
 '''
 calculates the center for given contours of an object in an image
 '''
-def calcCenter (contours):
-    M = cv2.moments(contours[0])
+
+
+def calcCenter(contour):
+    M = cv2.moments(contour)
     cx = int(M['m10'] / M['m00'])
     cy = int(M['m01'] / M['m00'])
 
     return cx, cy
 
+
 """
 gives us the bottom most point of an Object
 """
-def getBottom(cnt):
 
-    cnt = cnt[0]
-    bottom = tuple(cnt[cnt[:,:,1].argmax()][0])
+
+def getBottom(cnt):
+    cnt = cnt
+    bottom = tuple(cnt[cnt[:, :, 1].argmax()][0])
     print(bottom)
     return bottom
+
+
+def getBlobsGlobal(img):
+    imgCopy = img
+    points = []
+    for c in colors:
+        img = imgCopy
+        cnts = getContours(img, c)
+
+        for k in cnts:
+            points.append(getBottom(k))
+
+    return points
+
 
 '''
 Gets a raw cv2 image, process it and returns the contours of the given color boundaries 
 '''
+
+
 def getContours(image, boundaries):
     # create NumPy arrays from the boundaries
     lower = np.array(boundaries[0], dtype="uint8")
     upper = np.array(boundaries[1], dtype="uint8")
 
     # find the colors within the specified boundaries and apply the mask
+
     mask = cv2.inRange(image, lower, upper)
     output = cv2.bitwise_and(image, image, mask=mask)
 
     # process image
     imgray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    ret, thresh = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY)  # 127, 255, 0)
+
+    # cv2.imshow("THRresh", mask)
+    # cv2.waitKey(0)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     return contours
+
 
 '''
 This is the action to be called for the exercise 4 of the proseminar
@@ -78,6 +108,8 @@ This is the action to be called for the exercise 4 of the proseminar
 3. prints the center of the green blob and waits for the ESC-key 
 4. moves 0.1 meters and begins again at 1. until this loop happened 5 times
 '''
+
+
 def exercise4_action(clientID, youBotCam):
     counter = 1
 
@@ -91,12 +123,13 @@ def exercise4_action(clientID, youBotCam):
             cv2Image = convertToCv2Format(image, res)
             cv2ImageCopy = cv2Image
 
-            imageContours = getContours(cv2Image, boundariesGreen)
+            imageContours = getContours(cv2Image, boundariesBlue)
+
             cv2.drawContours(cv2ImageCopy, imageContours, 0, (255, 0, 0), 1)
             cv2.imshow("Current Image of youBot", cv2ImageCopy)
 
             # calculate center of green blob
-            x, y = calcCenter(imageContours)
+            x, y = calcCenter(imageContours[0])
 
             print("Current center of the green blob: x={} y={}".format(x, y))
 
